11dc94228e692904ff3b37e006c09fdcfaa80211
Vladimir <bobbydigital92@gmail.com>
2021-04-25 23:53:08 +0300

    extract components

--- 01-first-project/my-app/src/App.js
+++ 01-first-project/my-app/src/App.js
@@ -1,50 +1,17 @@
 import './App.css';
-import logo from './logo.png';
-import mainContentBg from './nyc-bg.jpg';
-import userImg from './og-cat.jpg';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile.jsx';
+
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img className="header-logo" src={logo}></img>
-      </header>
-      <nav className='navigation'>
-        <ul>
-          <li>
-            <a href="#">Profile</a>
-          </li>
-          <li>
-            <a href="#">Messages</a>
-          </li>
-          <li>
-            <a href="#">Music</a>
-          </li>
-          <li>
-            <a href="#">Settings</a>
-          </li>
-        </ul>
-      </nav>
-      <main className='main-content'>
-        <div>
-          <img className="main-content-jpg" src={mainContentBg}></img>
-        </div>
-        <div>
-          <img className="user-img" src={userImg}></img>
-        </div>
-        <div>My posts
-          <div>New post</div>
-          <div>
-            Post 1
-          </div>
-          <div>
-            Post 2
-          </div>
-        </div>
-      </main>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
-
 
 export default App;
---
+++ 01-first-project/my-app/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+
+
+const Navbar = () => {
+    return <nav className='navigation'>
+        <ul>
+            <li>
+                <a href="#">Profile</a>
+            </li>
+            <li>
+                <a href="#">Messages</a>
+            </li>
+            <li>
+                <a href="#">Music</a>
+            </li>
+            <li>
+                <a href="#">Settings</a>
+            </li>
+        </ul>
+    </nav>
+}
+
+export default Navbar
